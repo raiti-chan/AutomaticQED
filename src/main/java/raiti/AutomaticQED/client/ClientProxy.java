a4bb1d87e785beda85b6377b3b80ea6fde90533f
Raiti <rusuke@t-net.ne.jp>
2016-12-07 20:19:49 +0900

    Init Commit

---
+++ src/main/java/raiti/AutomaticQED/client/ClientProxy.java
@@ -0,0 +1,15 @@
+package raiti.AutomaticQED.client;
+
+import raiti.AutomaticQED.server.CommonProxy;
+
+/**
+ * クライアントサイドプロキシ―
+ * <br>Created by Raiti-chan on 2016/12/07.
+ *
+ * @author Raiti-chan
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public class ClientProxy extends CommonProxy {
+}
---
+++ src/main/java/raiti/AutomaticQED/server/CommonProxy.java
@@ -0,0 +1,13 @@
+package raiti.AutomaticQED.server;
+
+/**
+ * サーバーサイドプロキシ―クラス
+ * <br>Created by Raiti-chan on 2016/12/07.
+ *
+ * @author Raiti-chan
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class CommonProxy {
+	
+}
